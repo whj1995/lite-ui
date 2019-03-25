982bdb8ee4d453a7dcbac8da5f1263f1ff7390de
whj1995 <wu38607@live.com>
2019-03-25 17:34:36 +0800

    button className

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './style.styl';
 
-export function Button(props: React.DOMAttributes<HTMLButtonElement>) {
-  const { children, ...attrs } = props;
-  return <button className='lite-button' {...attrs} >{children}</button>;
+export function Button(props: React.DOMAttributes<HTMLButtonElement> & { className: string }) {
+  const { children, className, ...attrs } = props;
+  return <button className={`lite-button ${typeof className === 'undefined' ? '' : className}`} {...attrs} >{children}</button>;
 }
